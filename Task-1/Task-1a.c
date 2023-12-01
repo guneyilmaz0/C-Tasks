66e5b45c9f68fbe22e1b32923d96162f4eeff225
Güney Yılmaz <63880117+guneyilmaz0@users.noreply.github.com>
2023-12-01 17:45:26 +0300

    Update Task-1a.c

--- Task-1/Task-1a.c
+++ Task-1/Task-1a.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello World");
+int main() {
+    int n, x = 0, y = 1;
+    printf("Bir sayi giriniz.");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", x);
+        int z = x;
+        x = y;
+        y = z + x;
+    }
+
+    return 0;
 }
